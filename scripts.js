28fe4b31290f0225a0a4f4729b0542e34f06402c
apetrai <agjelo.petraj10@gmail.com>
2024-10-28 02:26:30 +0000

    Add contact page, add privacy notice

--- scripts.js
+++ scripts.js
@@ -13,3 +13,20 @@
   } else {
           document.getElementById('display').innerHTML = "Currently outside business hours. <br>  Monday - Friday | 09:30 - 17:30";
   }
+
+
+  // Check if the user has already agreed to the terms
+window.onload = function() {
+    if (!localStorage.getItem('termsAgreed')) {
+        document.getElementById('terms-popup').style.display = 'flex';
+    }
+}
+
+// Function to handle the "Agree" button click
+function agreeToTerms() {
+    localStorage.setItem('termsAgreed', 'true'); // Save agreement in local storage
+    document.getElementById('terms-popup').style.display = 'none'; // Hide the popup
+}
+
+
+
